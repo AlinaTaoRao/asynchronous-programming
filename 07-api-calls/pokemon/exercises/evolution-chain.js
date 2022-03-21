2f5ee94ea4ea0fd3763e43a56804217eaf383d58
Alina Rao <88941807+AlinaTaoRao@users.noreply.github.com>
2022-03-21 11:02:36 +0100

    Revert "sunday class"
    This reverts commit 831b05eab14cbda6e64ca550e253e217d481e401.

--- 07-api-calls/pokemon/exercises/evolution-chain.js
+++ 07-api-calls/pokemon/exercises/evolution-chain.js
@@ -15,7 +15,7 @@ export const evolutionChain = async (chainId = 1) => {
   // 'https://pokeapi.co/api/v2';
   // https://pokeapi.co/api/v2/evolution-chain/{id}/
   const URL = `${ORIGIN}/evolution-chain/${chainId}`;
-  // debugger;
+// debugger;
   // --- fetch the API data (this works!) ---
   const encodedURL = encodeURI(URL);
   const response = await fetch(encodedURL);
@@ -30,46 +30,32 @@ export const evolutionChain = async (chainId = 1) => {
 
   /* --- parse the data if the response was ok (this works!) ---*/
   const data = await response.json();
-  console.log(data);
+ console.log(data);
   // --- process the fetched data (if necessary) ---
   //  you do not need to use `await` below this comment
   //  you can refactor this to a separate logic function and test it
   // tricky one!  you will need to push all the species into an array
 
-  // way1: recursion key=== species
-  // const isObject = (value) => (Object(value) === value); // return true if value is an Object/Array
-  // const findSpecies = (data = {}) => {
-  //     let species = [];
-  //     if(isObject(data)) {
-  //         const keys = Object.keys(data);
-  //         for(const key of keys) {
-  //             let tempSpecies;
-  //             if (key === 'species') {
-  //                 tempSpecies = [ data[key] ];
-  //             } else {
-  //                 tempSpecies = findSpecies(data[key]);
-  //             }
-  //             // species = species.concat(tempSpecies);
-  //             species.push(...tempSpecies);
-  //         }
-  //     }
+  const isObject = (value) => (Object(value) === value); // return true if value is an Object/Array
+  const findSpecies = (data = {}) => {
+      let species = [];
+      if(isObject(data)) {
+          const keys = Object.keys(data);
+          for(const key of keys) {
+              let tempSpecies;
+              if (key === 'species') {
+                  tempSpecies = [ data[key] ];
+              } else {
+                  tempSpecies = findSpecies(data[key]);
+              }
+              // species = species.concat(tempSpecies);
+              species.push(...tempSpecies);
+          }
+      }
+      return species;
+  };
+  const pokemon = findSpecies(data).reverse();
+
   // --- return the final data ---
-  // return pokemon;
-
-  // way 2 : sander and aviv .length > 0
-
-  const pokemon = [];
-  pokemon.push(data.chain.species);
-  let key = data.chain.evolves_to;
-  while (key.length === 1) {
-    console.log('ping');
-    pokemon.push(key[0].species);
-    key = key[0].evolves_to;
-  }
-
-  //const keyNext = key[0].evolves_to
-  //console.log(key.length, keyNext.length)
-
-  console.log(pokemon);
   return pokemon;
 };
